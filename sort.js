9a3d0efe3bc206a0513e3f49cbbe5e37748bf6ba
buti111 <buti111@gmail.com>
2019-11-10 16:10:19 +0900

    add sort function

--- sort.js
+++ sort.js
@@ -1,3 +1,12 @@
-var nums = [1,2,3,4,10,12,9,8];
+var sortNumber = function(number) {
+	number.sort(function (a, b) {
+		if (a == b) {
+			return 0;
+		}
+		return a < b ? -1 : 1;
+	});
+};
 
+var nums = [1,2,3,4,10,12,9,8];
+sortNumber(nums);
 console.log(nums);
